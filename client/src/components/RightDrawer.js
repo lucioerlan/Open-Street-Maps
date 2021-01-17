ce70ac5c2b1630c244df27965c9020cadd93eb55
luciorox11@outlook.com <luciodevinfo@gmail.com>
2021-01-16 21:33:28 -0300

    refactor adding pattern

--- client/src/components/RightDrawer.js
+++ client/src/components/RightDrawer.js
@@ -1,67 +1,65 @@
-import React from "react";
-import Drawer from "@material-ui/core/Drawer";
-import { withStyles } from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
-
-import { availableThemes } from "../theme";
+import React from 'react';
+import { Drawer, Grid, withStyles } from '@material-ui/core';
+import { availableThemes } from '../themes';
 
 const style = () => ({
   BackdropRoot: {
-    backgroundColor: "transparent"
+    backgroundColor: 'transparent',
   },
   selectThemeBtn: {
-    backgroundColor: "rgb(250, 250, 250)",
-    color: "rgba(0, 0, 0, 0.87)",
-    border: "1px solid rgba(0, 0, 0, 0.12)",
-    height: "40px",
-    margin: "16px 0",
-    cursor: "pointer",
-    "&.hover": {
-      boxShadow: "inset 0 0 0 20px rgba(0,0,0,.24)"
-    }
-  }
+    backgroundColor: 'rgb(250, 250, 250)',
+    color: 'rgba(0, 0, 0, 0.87)',
+    border: '1px solid rgba(0, 0, 0, 0.12)',
+    height: '40px',
+    margin: '16px 0',
+    cursor: 'pointer',
+    '&.hover': {
+      boxShadow: 'inset 0 0 0 20px rgba(0,0,0,.24)',
+    },
+  },
 });
 
 const RightDrawer = ({
   rightDrawerOpen,
   handleChangeRightDrawer,
   handleChangeTheme,
-  classes
+  classes,
 }) => (
-    <Drawer
-      variant="temporary"
-      anchor={"right"}
-      open={rightDrawerOpen}
-      onClose={handleChangeRightDrawer}
-      ModalProps={{
-        BackdropProps: {
-          classes: { root: classes.BackdropRoot }
-        }
-      }}
-    >
-      <div style={{ width: 240, padding: 16 }}>
-        <h6 style={{ paddingBottom: "10px" }}>Set Theme</h6>
-        {availableThemes.map(theme => (
-          <div
-            key={theme.title}
-            onClick={() => handleChangeTheme(theme)}
-            className={classes.selectThemeBtn}
-          >
-            <div>{theme.title}</div>
-            <Grid container>
-              <Grid item xs={5}>
-                <div style={{ height: "10px", background: theme.primary[500] }} />
-              </Grid>
-              <Grid item xs={5}>
-                <div
-                  style={{ height: "10px", background: theme.secondary[500] }}
-                />
-              </Grid>
+  <Drawer
+    variant="temporary"
+    anchor="right"
+    open={rightDrawerOpen}
+    onClose={handleChangeRightDrawer}
+    ModalProps={{
+      BackdropProps: {
+        classes: { root: classes.BackdropRoot },
+      },
+    }}
+  >
+    <div style={{ width: 240, padding: 16 }}>
+      <h5 style={{ paddingBottom: '10px' }}>Change Theme</h5>
+      {availableThemes.map(theme => (
+        <div
+          key={theme.title}
+          onClick={() => handleChangeTheme(theme)}
+          className={classes.selectThemeBtn}
+          aria-hidden="true"
+        >
+          <div>{theme.title}</div>
+          <Grid container>
+            <Grid item xs={5}>
+              <div style={{ height: '10px', background: theme.primary[500] }} />
             </Grid>
-          </div>
-        ))}
-      </div>
-    </Drawer>
-  );
+            <Grid item xs={5}>
+              <div
+                style={{ height: '10px', background: theme.secondary[500] }}
+              />
+            </Grid>
+          </Grid>
+        </div>
+      ))}
+    </div>
+  </Drawer>
+);
 
 export default withStyles(style)(RightDrawer);
